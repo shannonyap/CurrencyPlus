aa41190d304dc276688d56307dca648d17759dd7
Shannon Yap <shannonyap@utexas.edu>
2016-06-23 23:23:45 -0500

    Added JBChartView framework to support graphs

---
+++ CurrencyPlus/JBChartBridgingHeader.m
@@ -0,0 +1,13 @@
+//
+//  JBChartBridgingHeader.m
+//  CurrencyPlus
+//
+//  Created by Shannon Yap on 6/23/16.
+//  Copyright © 2016 SYXH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "JBChartView.h"
+#import "JBBarChartView.h"
+#import "JBLineChartView.h"
